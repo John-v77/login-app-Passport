091c198363969dcbd13f1c213ef76f5f14656070
John-v77 <ionut_vasile85@hotmail.com>
2021-05-06 14:05:58 -0400

    passport set up

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const initializePassport = require('./passport-config')
 
+const initialize = require('./passport-config')
+initializePassport(
+    passport, 
+    email => {return users.find(user => user.email === email)})
+
+const users = []
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
@@ -18,5 +27,22 @@ app.get('/register', (req, res) =>{
     res.render('register.ejs')
 })
 
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = bycrypt.hash(req.body.password, 10)
+        
+        // data bases will do this field automatically 
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log('users',users)
+})
 
 app.listen(3000)
