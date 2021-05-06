218f053f3f1c4ec6b04ecb86b90a53130a6de79b
John-v77 <ionut_vasile85@hotmail.com>
2021-05-06 11:43:58 -0400

    login and register page - front-end

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
 
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+
+
 app.get('/', (req, res) => {
-    res.render('index.ejs')
- 
+    res.render('index.ejs', { user: 'John' })
 })
+
+app.get('/login', (req, res) =>{
+    res.render('login.ejs')
+})
+
+app.get('/register', (req, res) =>{
+    res.render('register.ejs')
+})
+
 
 app.listen(3000)
